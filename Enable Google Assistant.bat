9bb5b9a41a8ac071773318bc7c8304e1e9c16342
erictbar <contact@erictb.com>
2023-07-17 07:46:37 -0400

    Update Enable Google Assistant.bat

--- Enable Google Assistant.bat
+++ Enable Google Assistant.bat
@@ -3,11 +3,11 @@
 ::%LOCALAPPDATA%\Microsoft\WindowsApps\MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe\WSAClient.exe
 ::%LOCALAPPDATA%\Microsoft\WindowsApps\MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe\WSAClient.exe /launch wsa://com.google.android.apps.google
 adb start-server
-:: @adb -s 192.168.50.216:58526 %*
-adb connect 192.168.50.216:58526 > nul
-::adb -s 192.168.50.216:58526 shell am kill -n com.google.android.apps.google
+:: @adb -s 127.0.0.1:58526 %*
+adb connect 127.0.0.1:58526 > nul
+::adb -s 127.0.0.1:58526 shell am kill -n com.google.android.apps.google
 
-adb -s 192.168.50.216:58526 shell settings put --user 0 secure voice_interaction_service com.google.android.googlequicksearchbox/com.google.android.voiceinteraction.GsaVoiceInteractionService
+adb -s 127.0.0.1:58526 shell settings put --user 0 secure voice_interaction_service com.google.android.googlequicksearchbox/com.google.android.voiceinteraction.GsaVoiceInteractionService
 :: %LOCALAPPDATA%\Microsoft\WindowsApps\MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe\WSAClient.exe /launch wsa://com.google.android.apps.googleassistant
-adb -s 192.168.50.216:58526 shell monkey -p com.google.android.apps.googleassistant -v 1
+adb -s 127.0.0.1:58526 shell monkey -p com.google.android.apps.googleassistant -v 1
 adb kill-server
